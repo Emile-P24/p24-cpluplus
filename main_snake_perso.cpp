609c942340a5f913ca07644df20abe617ed2b037
Emile Thompson <emile.thompson@etu.minesparis.psl.eu>
2024-12-10 17:01:58 +0100

    début du jeu snake

--- main_snake_perso.cpp
+++ main_snake_perso.cpp
@@ -5,38 +5,51 @@
 #include <string>
 #include <cstdlib> // pour exit et rand
 
-vector<char> board(35) ;
-// board ={.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.}
-
-void reset_board() {        // qui ré-initialise le board
-
-}
-void generate_apple() {     // qui génère une pomme
-
-}
-void add_snake_to_board() { // qui met le serpent sur le board
-
-}
-void add_apple_to_board() { // qui met la pomme sur le board
-
+int simple_random(int n) // simple random generation of a integer between 0 and n
+{
+    return rand() % n;
 }
 
+std::vector<char> myBoard(35,'.');
+std::vector<std::pair<int,int>> snake = {{1,1},{1,2},{1,3}};
+
+void reset_board()
+{
+    for (int i = 0; i <= 35; ++i) {
+        myBoard[i]='.';
+    }
+}
+
+void generate_apple()
+{ // qui génère une pomme
+    myBoard[simple_random(35)]='@';
+}
+
+void add_snake_to_board() // initialisé en haut à gauche
+{ // qui met le serpent sur le board
+    myBoard[(snake[0].first-1)*7 + snake[0].second-1] = 'O';
+    for (int i=1; i< snake.size();i++){
+        myBoard[(snake[i].first-1)*7 + snake[i].second-1] = 'o';
+    }
+
+    
+}
+void add_apple_to_board()
+{ // qui met la pomme sur le board
+        myBoard[21]='@';
+
+
+}
 
 void draw_board()
-{ // qui affiche le board
-    // bien sûr c'est n'importe quoi cette fonction qui affiche des '.' sur 5 lignes et 7 colonnes...
-    // Il faut repenser tout le programme pour afficher le board que vous avez créé en début de partie (de la taille désirée)
-    // et dans lequel vous aurez mis le serpent et la pomme.
-    // Vous pouvez supprimer ou rajouter des fonctions et surtout faire des classes.
-    int i {0};
-    for (char c : board){
-    std::cout<<c;
-    if (i%7 == 0){
-        std::cout<<"/n";
+{
+    for (int i = 0; i <= 35; ++i) {
+        if (i % 7 == 0)
+        {
+            std::cout<<std::endl;
+        }
+        std:: cout << myBoard[i];
     }
-    i += 1;
-    }
-
 }
 
 void print_message(const std::string &msg) // qui affiche un message
@@ -67,16 +80,32 @@ void play_game()
         draw_board();         // on affiche le board
         print_message("what do you want to do ('q' to quit):");
 
+        std::vector<std::pair<int,int>> snakeCopy = snake;
+        std::vector<char> myBoardCopy = myBoard;
         std::cin >> key; // on attend que l'utilisateur entre un caractère au clavier
         if (key)
         {
             if ((key == 'i') or (key == 'k') or (key == 'j') or (key == 'l'))
             {
-                // on sait calculer la case dans laquelle la tête du serpent doit aller
-                //    si il y a une pomme dans cette case: il la mange et il grandit
-                //    si il se mange lui-même ou sort du board: on quitte le jeu
-                //    sinon on déplace le serpent
-                // et on recommence la boucle sans fin
+                if (key == 'i'){
+                    snake.insert(snake.begin(), {snake[0].first-1, snake[0].second});
+                    snake.pop_back();
+                }
+
+                if (key == 'j'){
+                    snake.insert(snake.begin(), {snake[0].first, snake[0].second-1});
+                    snake.pop_back();
+                }
+
+                if (key == 'k'){
+                    snake.insert(snake.begin(), {snake[0].first+1, snake[0].second});
+                    snake.pop_back();
+                }
+
+                if (key == 'l'){
+                    snake.insert(snake.begin(), {snake[0].first, snake[0].second+1});
+                    snake.pop_back();
+                }
             }
             else if (key == 'q')
             {
@@ -89,25 +118,24 @@ void play_game()
             // si l'utilisateur tape 's', vous pouvez sauver l'état du jeu dans un fichier pour le reprendre plus tard
             // et passer le fichier en argument à votre programme pour reprendre le jeu
         }
+        if ( myBoardCopy[(snake[0].first-1)*7 + snake[0].second-1] == '@'){
+            snake.push_back(snakeCopy[-1]);
+            generate_apple();
+
+        }
     }
 }
 
-int simple_random(int n) // simple random generation of a integer between 0 and n
-{
-    return rand() % n;
-}
 
-int main(int argc, char **argv)
+int main()
 {
     // exemple où on génère un nombre aléatoire entre 0 et 35
     // (par exemple cela vous donne une case sur un board de 5 lignes 7 colonnes pour y placer une pomme
     //  si il n'y a pas de bout de serpent à cet endroit...)
-    std::cout << simple_random(35) << std::endl;
-    draw_board();
     // Il manque tous les objets (le board, le serpent, la pomme).
     // Dans un premier temps, vous pouvez utiliser des variables globales pour stocker ces objets
     //                        que vous manipulez alors directement dans des fonctions
     // dans un second temps, vous devez proposer une structure de classes
-    // play_game();
-    return 0;
+    play_game();
+    return EXIT_SUCCESS;
 }
