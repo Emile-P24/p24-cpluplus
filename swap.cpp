cfe064aad5a42e656af68c5294796d2914efa5b5
Emile Thompson <emile.thompson@etu.minesparis.psl.eu>
2024-12-10 14:08:35 +0100

    code de swap avec référence

--- swap.cpp
+++ swap.cpp
@@ -1,22 +1,39 @@
 #include <iostream>
 
-float swap( float* pa, float* pb){
-    float t = *pa;
-    *pa = *pb;
-    *pb = t;
-    return *pa,*pb ;
+
+// float swap( float* pa, float* pb){
+//     float t = *pa;
+//     *pa = *pb;
+//     *pb = t;
+//     return *pa,*pb ;
+// }
+// int main(){
+//     float a{7.24}; // <=> a=7.24
+//     float b{1.23};
+//     float* pa = &a; // pa est une adresse mémoire = pointeur
+//     float* pb = &b;
+//     // a = (*pa);  // Accéder à la valeur de pa
+//     // int a = 82;
+//     // int b = 12;
+//     swap(pa,pb);
+//     std::cout<<"a="<<(*pa)<<" "<<"b="<<(*pb)<<std::endl;
+//     return EXIT_SUCCESS;
+// }
+
+
+// Nouvelle version avec la référence
+void swap( float& ra, float& rb){
+    float t = ra;
+    ra = rb;
+    rb = t;
 }
-
-
 int main(){
     float a{7.24}; // <=> a=7.24
     float b{1.23};
-    float* pa = &a; // pa est une adresse mémoire = pointeur
-    float* pb = &b;
-    // a = (*pa);  // Accéder à la valeur de pa
-    // int a = 82;
-    // int b = 12;
-    swap(pa,pb);
-    std::cout<<"a="<<(*pa)<<" "<<"b="<<(*pb)<<std::endl;
+    float& ra = a; // ra est une référence
+    float& rb = b;
+    
+    swap(ra,rb);
+    std::cout<<"a="<<(ra)<<" "<<"b="<<(rb)<<std::endl;
     return EXIT_SUCCESS;
 }
